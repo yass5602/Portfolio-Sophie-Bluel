deaaefbf59672d0d3e46b220b51d6fc11ef8f99b
yass5602 <yasinchk57@gmail.com>
2025-02-27 23:29:43 +0100

    Ajout du fichier app.jspuis connexion Backend avec fetch

---
+++ FrontEnd/Javascript/app.js
@@ -0,0 +1,16 @@
+async function getWorks() {
+    const url = "http://localhost:5678/api/works";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+getWorks();
