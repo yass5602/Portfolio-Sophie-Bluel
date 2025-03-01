0dbd779a4b3f4a2c216c46b7c03af4b555bac871
yass5602 <yasinchk57@gmail.com>
2025-03-01 01:33:29 +0100

    Déplacement de la galerie du HTML vers le backend grâce à la fonction setFigure(data) dans getWorks

--- FrontEnd/Javascript/app.js
+++ FrontEnd/Javascript/app.js
@@ -8,9 +8,20 @@ async function getWorks() {
   
       const json = await response.json();
       console.log(json);
+      for (let i = 0; i < json.length; i++) {
+        setFigure(json[i]);
+      }
     } catch (error) {
       console.error(error.message);
     }
 }
 
 getWorks();
+
+function setFigure(data) {
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+                        <figcaption>${data.title}</figcaption>`;
+                        
+    document.querySelector(".gallery").append(figure);
+}
